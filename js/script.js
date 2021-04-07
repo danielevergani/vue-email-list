ca8f78ce242f590895f0795d677a4f1f8fbae061
danielevergani <dvergani94@gmail.com>
2021-04-07 15:24:25 +0200

    lista di 10 email

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ var app = new Vue(
             emailList: []
         },
         mounted: function () {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then( (response) => {
-                console.log(response.data.response);
-                this.emailList.push(response.data.response);
-                console.log(this.emailList);
-            });
+            for ( let i = 0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then( (response) => {
+                    this.emailList.push(response.data.response);
+                });
+            }
         }
         
     }
